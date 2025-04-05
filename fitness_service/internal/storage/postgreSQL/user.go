4d0e491dfc31f18096f0cdc69c8cbc7c1fefd17c
LongDude <teplov.vladimir.2004@mail.ru>
2025-04-05 11:03:37 +0300

    update: update

--- fitness_service/internal/storage/postgreSQL/user.go
+++ fitness_service/internal/storage/postgreSQL/user.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 )
 
-// TODO sql
-
 // GetUser implements Repository
 func (r *Queries) GetUser(
 	ctx context.Context,
@@ -62,7 +60,46 @@ func (r *Queries) UpdateUser(
 ) {
 	sqlStatement := `UPDATE users SET user_firstName=$2, user_lastName=$3, user_middleName=$4, user_birthday=$5, user_height=$6, user_weight=$7, user_fitness_target=$8, user_sex=$9, user_hypertain=$10, user_diabet=$11, user_level=$12 WHERE user_id=$1`
 
-	_, err := r.pool.Exec(
+	old_user := &models.User{}
+
+	old_user, err := r.GetUser(ctx, user_id)
+	if err != nil {
+		return nil, err
+	}
+
+	if user_firstName == nil {
+		user_firstName = &old_user.User_firstName
+	}
+
+	if user_middleName == nil {
+		user_middleName = old_user.User_middleName
+	}
+	if user_birthday == nil {
+		user_birthday = &old_user.User_birthday
+	}
+	if user_height == nil {
+		user_height = &old_user.User_height
+	}
+	if user_weight == nil {
+		user_weight = &old_user.User_weight
+	}
+	if user_fitness_target == nil {
+		user_fitness_target = &old_user.User_fitness_target
+	}
+	if user_sex == nil {
+		user_sex = &old_user.User_sex
+	}
+	if user_hypertain == nil {
+		user_hypertain = &old_user.User_hypertain
+	}
+	if user_diabet == nil {
+		user_diabet = &old_user.User_diabet
+	}
+	if user_level == nil {
+		user_level = old_user.User_level
+	}
+
+	_, err = r.pool.Exec(
 		ctx,
 		sqlStatement,
 		user_id,
@@ -78,6 +115,7 @@ func (r *Queries) UpdateUser(
 		user_diabet,
 		user_level,
 	)
+
 	if err != nil {
 		return nil, fmt.Errorf("can`t update user profile: %w", err)
 	}
