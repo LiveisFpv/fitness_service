d31ac9af474f1fef3f87a5b57bc3952101eb1b7c
LongDude <loongdude@inbox.ru>
2025-04-06 03:36:01 +0300

    brain too melted to think

--- fitness_service/internal/storage/postgreSQL/plan.go
+++ fitness_service/internal/storage/postgreSQL/plan.go
@@ -9,8 +9,17 @@ import (
 
 // TODO
 func (r *Queries) GetPlan(ctx context.Context, user_id int, date time.Time) (*models.Plan, error) {
+	sqlStatement_trainings := "SELECT "
+
 	return nil, fmt.Errorf("not implemented")
 }
+
+// TODO
 func (r *Queries) GetDayPlan(ctx context.Context, user_id int, date time.Time) (*models.DayPlan, error) {
 	return nil, fmt.Errorf("not implemented")
+}
+
+func (r *Queries) GetTrainingListByWeek(ctx context.Context, user_id int, date time.Time) {
+	sqlStatement := "SELECT * from training t JOIN train_plan p on p.training_id = t.training_id WHERE p.user_id = $1 and t.date BETWEEN $2::date AND ($2::date + INTERVAL '6 days')"
+
 }
